879c294c8528beb218fe93c37480e017317ecd35
yummyhomie <eleedee@outlook.com>
2024-08-12 22:54:18 -0600

    Did a lot.

---
+++ tailwind-files/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../index.html",
+            "../project-files/about.html",
+            "../project-files/contact.html",
+            "../project-files/meetings.html",
+            "../project-files/members.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        arcade: ['Arcade'],
+        tommy: ['Tommy']
+      },
+      colors: {
+        'csc-gray': '#F5F5F5',
+      },
+    },
+  },
+  plugins: [],
+}
+
